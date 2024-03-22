c3917f069482dd95a8065aa71e2f61991e8ccfe2
TheGrandNobody <thegrandnobody@protonmail.com>
2024-03-22 18:07:30 +0100

    Implement international school

--- gather.py
+++ gather.py
@@ -12,7 +12,22 @@ from webdriver_manager.firefox import GeckoDriverManager
 from selenium.common.exceptions import TimeoutException
 import time
 
-URL = 'https://scholenopdekaart.nl/zoeken/middelbare-scholen?zoektermen=Rotterdam&weergave=Lijst'
+URL = 'https://scholenopdekaart.nl/zoeken/basisscholen?zoektermen=Groningen&weergave=Lijst'
+TARGET = "basisscholen/groningen"
+
+COUNTRY = 'Netherlands'
+
+def init_driver() -> webdriver.Firefox:
+    """ Initialize a Selenium webdriver for Firefox.
+
+    Returns:
+        webdriver.Firefox: The Selenium webdriver object.
+    """
+    options = FirefoxOptions()
+    options.add_argument("--headless")  # This line enables headless mode
+    service = FirefoxService(executable_path=GeckoDriverManager().install())
+    driver = webdriver.Firefox(service=service, options=options)
+    return driver
 
 def fetch_url_static(url: str, curl=None) -> tuple[str, pycurl.Curl]:
     """ Fetches the page content of a URL using pycurl. Much faster for static pages.
@@ -53,7 +68,7 @@ def fetch_url_dynamic(url: str, driver: webdriver.Chrome | webdriver.Firefox, so
     searching = True
     driver.get(url)
     if source:
-        wait = WebDriverWait(driver, 2)  # Wait up to 10 seconds
+        wait = WebDriverWait(driver, 2)  # Wait up to 2 seconds
         try:
             # Click the cookie consent button
             cookie_consent_button = wait.until(EC.element_to_be_clickable((By.ID, 'CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll')))
@@ -76,11 +91,13 @@ def fetch_url_dynamic(url: str, driver: webdriver.Chrome | webdriver.Firefox, so
         time.sleep(3)
     return page_source
 
-def get_school_links(url: str, source=None) -> list[str]:
+def get_school_links(url: str, target: str, link: str, source=None) -> list[str]:
     """ Get the links to the school pages from the main page.
 
     Args:
         url (str): The URL to fetch the content from.
+        target (str): The target string to match in the href attribute.
+        link (str): The link to append to the base URL.
         source (str, optional): The page source. Defaults to None.
 
     Returns:
@@ -92,8 +109,8 @@ def get_school_links(url: str, source=None) -> list[str]:
     school_links = []
     for link in soup.find_all('a'):
         href = link.get('href')
-        if href and 'middelbare-scholen/rotterdam' in href:  # Adjusted to match your condition
-            school_links.append(f"https://scholenopdekaart.nl{href}contact")
+        if href and target in href:  # Adjusted to match your condition
+            school_links.append(link.replace("href", href))
     return school_links
 
 def extract_emails_from_school_page(url: str, c=None, driver=None) -> tuple[set[str], pycurl.Curl | None]:
@@ -126,33 +143,89 @@ def extract_emails_from_school_page(url: str, c=None, driver=None) -> tuple[set[
 def main() -> None:
     """ Main function to run the email gathering bot.
     """
-    # Initialize variables
-    curl, driver = None, None
-
     # Check if the page is to be fetched using Selenium or pycurl
-    if len(sys.argv) != 2:
-        print(f"Usage: {sys.argv[0]} True | False")
+    if len(sys.argv) != 3:
+        print(f"Usage: {sys.argv[0]} (True | False) (-i | -p)")
         sys.exit(1)
-    if sys.argv[1].lower() == 'true':
-        options = FirefoxOptions()
-        options.add_argument("--headless")  # This line enables headless mode
-        service = FirefoxService(executable_path=GeckoDriverManager().install())
-        driver = webdriver.Firefox(service=service, options=options)
-        page_source = fetch_url_dynamic(URL, driver, True)
-        school_links = get_school_links(URL, page_source)
-    else:
-        school_links = get_school_links(URL)
-    all_emails = set()
 
-    # Extract emails from the school pages
-    for school_url in school_links:
-        if not all_emails:
-            emails, curl = extract_emails_from_school_page(school_url, driver=driver)
+    # Initialize variables
+    curl, driver, all_emails = None, None, set()
+
+    # Fetch emails for native public schools
+    if sys.argv[2].lower() == '-p':
+        if sys.argv[1].lower() == 'true':
+            driver = init_driver()
+            page_source = fetch_url_dynamic(URL, driver, True)
+            school_links = get_school_links(URL, TARGET, "https://scholenopdekaart.nlhrefcontact", page_source)
         else:
-            emails, curl = extract_emails_from_school_page(school_url, curl, driver=driver)
-        if emails:
-            all_emails.update(emails)
+            school_links = get_school_links(URL, TARGET, "https://scholenopdekaart.nlhrefcontact",)
 
+        # Extract emails from the school pages
+        for school_url in school_links:
+            if not all_emails:
+                emails, curl = extract_emails_from_school_page(school_url, driver=driver)
+            else:
+                emails, curl = extract_emails_from_school_page(school_url, curl, driver=driver)
+            if emails:
+                all_emails.update(emails)
+    # Otherwise fetch emails for international schools
+    else:
+        failed = []
+        # Initialize a Mozilla Firefox webdriver
+        driver = init_driver()
+        # Fetch the page source
+        page_source = fetch_url_dynamic(f"https://www.international-schools-database.com/country/{COUNTRY.lower()}", driver, True)
+        # Get the links to the school pages
+        page_links = get_school_links("", TARGET, "href", page_source)
+        # Get the links to the school websites
+        school_links = []
+        for link in page_links:
+            soup = BeautifulSoup(fetch_url_dynamic(link, driver), 'html.parser')
+            a_tag = soup.find('a', title="School's webpage")
+            if a_tag:
+                href_value = a_tag['href']
+                school_links.append(href_value)
+        # Extract emails from the school pages
+        for url in school_links:
+            contact_url, failed_contact = None, False
+            # Try to find a contacts page
+            soup = BeautifulSoup(fetch_url_dynamic(url, driver), 'html.parser')
+            links = soup.find_all('a')
+            for link in links:
+                href = link.get('href')
+                if href and 'contact' in href:
+                    contact_url = href
+                    # Extract emails from the contact page
+                    soup2 = BeautifulSoup(fetch_url_dynamic(contact_url, driver), 'html.parser')
+                    # Define a function to use as a filter for info emails
+                    def has_at_in_href(tag):
+                        return tag.name == 'a' and tag.has_attr('href') and 'info' in tag['href']
+                    email = soup2.find(has_at_in_href)
+                    if email:
+                        all_emails.add(email['href'].replace('mailto:', ''))
+                    else:
+                        # Define a function to use as a filter for directors
+                        def has_at_in_href(tag):
+                            return tag.name == 'a' and tag.has_attr('href') and 'dir' in tag['href']
+                        email = soup2.find(has_at_in_href)
+                        if email:
+                            all_emails.add(email['href'].replace('mailto:', ''))
+                        else:
+                            failed_contact = True
+                    break
+            # If there is no contact page, try extracting emails from the main page
+            if not contact_url or failed_contact:
+                # Define a function to use as a filter
+                def has_at_in_href(tag):
+                    return tag.name == 'a' and tag.has_attr('href') and '@' in tag['href']
+                a_tag = soup.find(has_at_in_href)
+                if a_tag:
+                    email = a_tag['href'].replace('mailto:', '')
+                    all_emails.add(email)
+                # Otherwise add the URL to the failed list (for manual inspection)
+                else:
+                    failed.append(url)
+                    
     # Close the pycurl object or the Selenium webdriver
     if curl:
         curl.close()
